927b192f675ede9cf559094845f7415b7391d491
Fudgey <fudgebucket27@gmail.com>
2023-06-09 08:52:36 +1000

    split out verify to another test stub

--- PoseidonTests/VerifyTests.cs
+++ PoseidonTests/VerifyTests.cs
@@ -9,14 +9,14 @@ using System.Text;
 namespace PoseidonTests
 {
     [TestClass]
-    public class SigningTests
+    public class VerifyTests
     {
         private static string PrivateKey = "0xff2f95f7f25dd17d160595603d49f9bd0bae765403d5d171fe1db2a3218c91"; // This private key has been unpaired from the real account
         private static string PrivateKey2 = "0x19bd8d52d552d2f112b478686f18577b8088e5b1860c3523c53f943304951c3"; //This private key has been unpaired from the real account
         private static string PrivateKey3 = "0x4485ade3c570854e240c72e9a9162e629f8e30db4d8130856da31787e7400f0"; //This private key has been unpaired from the real account
         [TestMethod]
         [Description("Hash and sign with first private key, it generates a negative big integer from the private key and generates a byte array of 32")]
-        public void PoseidonEddsaTest1() 
+        public void PoseidonEddsaVerifyTest1() 
         {
             int MAX_INPUT = 13;
             Poseidon poseidon = new Poseidon(MAX_INPUT + 1, 6, 53, "poseidon", 5, _securityTarget: 128);
@@ -26,11 +26,22 @@ namespace PoseidonTests
             Eddsa eddsa = new Eddsa(poseidonHash, PrivateKey);
             string signedMessage = eddsa.Sign();
             Assert.AreEqual("0x001306880e5a09076cba1e29a36fc4802be32338e0e6a922094ac2417662e14320a95f7f56d3e6241b6191344f1f9ee69dbc60ea244a06ded7c7b22c55c0af402fd957e3e2bd506741319064ec0e76ab44db7c0da08e8821137f09a6d30f0d68", signedMessage, "Signed messages don't match!");
+
+            //Verify Correct Key Is Used
+            Signature signatureObject = EddsaHelper.SignatureStringToSignatureObject(signedMessage);
+            BigInteger privateKeyBigInteger = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey);
+            SignedMessage verifySignedMessage = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigInteger), signatureObject, poseidonHash);
+            Assert.IsTrue(eddsa.Verify(verifySignedMessage));
+
+            //Verify Incorrect Key Is used
+            BigInteger privateKeyBigIntegerIncorrect = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey2);
+            SignedMessage verifySignedMessageIncorrect = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigIntegerIncorrect), signatureObject, poseidonHash);
+            Assert.IsFalse(eddsa.Verify(verifySignedMessageIncorrect));
         }
 
         [TestMethod]
         [Description("Hash and sign with second private key, generates a positive big integer from the private key and generates a byte array of 31")]
-        public void PoseidonEddsaTest2()
+        public void PoseidonEddsaVerifyTest2()
         {
             int MAX_INPUT = 13;
             Poseidon poseidon = new Poseidon(MAX_INPUT + 1, 6, 53, "poseidon", 5, _securityTarget: 128);
@@ -40,11 +51,21 @@ namespace PoseidonTests
             Eddsa eddsa = new Eddsa(poseidonHash, PrivateKey2);
             string signedMessage = eddsa.Sign();
             Assert.AreEqual("0x0d24a82d5d6a0548bcae75ca35d42220d314a9a7534c033282e2406ebb76464808ec18c987f64b817cee1eabb5b66df973daf1f240b0fb76de439896eb1d097601f3660c07cbe3fe3b48feab92e5962aa4fc6de16b1236eb7d98eb75bc5b66c7", signedMessage, "Signed messages don't match!");
+            //Verify Correct Key Is Used
+            Signature signatureObject = EddsaHelper.SignatureStringToSignatureObject(signedMessage);
+            BigInteger privateKeyBigInteger = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey2);
+            SignedMessage verifySignedMessage = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigInteger), signatureObject, poseidonHash);
+            Assert.IsTrue(eddsa.Verify(verifySignedMessage));
+
+            //Verify Incorrect Key Is used
+            BigInteger privateKeyBigIntegerIncorrect = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey);
+            SignedMessage verifySignedMessageIncorrect = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigIntegerIncorrect), signatureObject, poseidonHash);
+            Assert.IsFalse(eddsa.Verify(verifySignedMessageIncorrect));
         }
 
         [TestMethod]
         [Description("Hash and sign with third private key, generates a positive big integer from the private key and generates a byte array of 32")]
-        public void PoseidonEddsaTest3()
+        public void PoseidonEddsaVerifyTest3()
         {
             int MAX_INPUT = 1;
             Poseidon poseidon = new Poseidon(MAX_INPUT + 1, 6, 53, "poseidon", 5, _securityTarget: 128);
@@ -54,6 +75,16 @@ namespace PoseidonTests
             Eddsa eddsa = new Eddsa(poseidonHash, PrivateKey3);
             string signedMessage = eddsa.Sign();
             Assert.AreEqual("0x2548f1aa374db001ac03fc9e113a9ba0fddd84070acc9fb47e2aa22d1573c0fc1d58cd798ef835d0d8d24f6ef49402e25d3f14604e142cc575a4199bce62ee4e18477876f7993ff9e109c48f88f9ec25f2251635edb56dd6bb51f512b0a2203d", signedMessage, "Signed messages don't match!");
+            //Verify Correct Key Is Used
+            BigInteger privateKeyBigInteger = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey3);
+            Signature signatureObject = EddsaHelper.SignatureStringToSignatureObject(signedMessage);
+            SignedMessage verifySignedMessage = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigInteger), signatureObject, poseidonHash);
+            Assert.IsTrue(eddsa.Verify(verifySignedMessage));
+
+            //Verify Incorrect Key Is used
+            BigInteger privateKeyBigIntegerIncorrect = EddsaHelper.PrivateKeyHexStringToBigInteger(PrivateKey);
+            SignedMessage verifySignedMessageIncorrect = new SignedMessage(EddsaHelper.CalculatePointA(privateKeyBigIntegerIncorrect), signatureObject, poseidonHash);
+            Assert.IsFalse(eddsa.Verify(verifySignedMessageIncorrect));
         }
 
     }
