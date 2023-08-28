d6d6aabf87fbd348f9e7338dff022da9e5f0657e
Fudgey <fudgebucket27@gmail.com>
2023-08-29 04:30:36 +1000

    getting there

--- PoseidonConsole/Program.cs
+++ PoseidonConsole/Program.cs
@@ -12,12 +12,28 @@ namespace PoseidonConsole
     {
         static void Main(string[] args)
         {
-            Poseidon poseidon = new Poseidon(13 + 1, 6, 53, "poseidon", 5, _securityTarget: 128);
-            BigInteger[] inputs = { BigInteger.Parse("15262223708097584402615283257936266522564860189809682357548133077263290491192") };
-            BigInteger poseidonHash = poseidon.CalculatePoseidonHash(inputs);
-            Eddsa eddsa = new Eddsa(poseidonHash, "0xff2f95f7f25dd17d160595603d49f9bd0bae765403d5d171fe1db2a3218c91");
-            string signedMessage = eddsa.Sign();
 
+            BigInteger[] inputs = {
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111"),
+                BigInteger.Parse("11111111111111111111111111111111111111111111111111111111111111111111111111")
+            };
+            Poseidon poseidon = new Poseidon(inputs.Length + 1, 6, 53, "poseidon", 5, _securityTarget: 128);
+
+            BigInteger poseidonHash = poseidon.CalculatePoseidonHash(inputs);
+            Eddsa eddsa = new Eddsa(poseidonHash, "0x4485ade3c570854e240c72e9a9162e629f8e30db4d8130856da31787e7400f0");
+            string signedMessage = eddsa.Sign();
+            Console.WriteLine(signedMessage);
 
 
             Console.WriteLine("Enter to exit");
